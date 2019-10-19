741af59cb2151aff5082d50fc825c9e1637175fc
Olga Bolotova <olbolot@gmail.com>
2019-10-19 13:33:37 +0300

    Fix form validation

--- src/scripts/sendForm.js
+++ src/scripts/sendForm.js
@@ -1,39 +1,88 @@
+import Inputmask from 'inputmask';
 import Validate from './validate';
 
 export default class {
     constructor() {
-
+        this.phoneMask = '+7 999 99-99-99';
     }
 
     init(options) {
         this.form = options.form;
+        this.inputs = this.form.querySelectorAll('.js-input');
+        this.action = this.form.action;
+        this.method = this.form.method;
 
-        const validate = new Validate();
-
-        const validateResult = validate.init({
-            inputs: this.form.querySelectorAll('.js-input')
+        this.inputs.forEach(input => {
+            if (input.name === 'phone') {
+                this._putPhoneMask(input)
+            }
         });
 
-        this._bindEvents(validateResult);
+        this._bindEvents(this.inputs);
     }
 
-    _bindEvents(validateResult) {
+    _bindEvents(inputs) {
+        const validate = new Validate();
+
         this.form.addEventListener('submit', e => {
             e.preventDefault();
+            let result = true;
 
-            for (const field in validateResult) {
-                if (validateResult[field] === false) {
-                    console.log(validateResult[field]);
-                    
-                    return;
+            const valResult = validate.init({
+                inputs: inputs
+            });
+
+            for (const field in valResult) {
+                if (valResult[field] === false) {
+                    result = false;
                 }
             }
 
-            this._sendForm();
+            if (result === true) {
+                this._sendForm();
+            }
         })
     }
 
+    _putPhoneMask(input) {
+        const inputmask = new Inputmask({
+            'mask': this.phoneMask,
+            showMaskOnHover: false
+        });
+
+        inputmask.mask(input);
+    }
+
     _sendForm() {
-        console.log("Отправляем!");
+        const formData = new FormData(this.form);
+
+        fetch(this.action, {
+            method: this.method,
+            headers: { 'X-Requested-With':'XMLHttpRequest' },
+            body: formData
+        })
+            .then(this._checkStatus)
+            .then(response => response.json())
+            .then(message => {
+            console.log(message);
+            this._clearForm();
+        })
+            .catch(error => {
+            console.log(error);
+        });
+    }
+
+    _checkStatus(response) {
+        if (response < 400) {
+            return response;
+        } else {
+            throw new Error(response.status);
+        }
+    }
+
+    _clearForm() {
+        this.inputs.forEach(input => {
+            input.value = '';
+        })
     }
 }
