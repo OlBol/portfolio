bba4fc5cbcc92a6ec06114e27e09d0ea28ae7189
Olga Bolotova <olbolot@gmail.com>
2020-01-14 15:33:12 +0300

    Fix the review slider

--- src/scripts/reviewSlider.ts
+++ src/scripts/reviewSlider.ts
@@ -13,6 +13,7 @@ export default function reviewSlider() {
     let targetBtn: string = null;
 
     changeBtnState();
+    trackSliderResizing();
 
     scrollBtnToPrev.addEventListener('click', () => {
         start = null;
@@ -52,19 +53,36 @@ export default function reviewSlider() {
      * @param time {number} - animation start time.
      */
     function countStep(time: number) {
-        if (targetBtn === 'next' && -sliderPosition < (currentSlide - 1) * slider.offsetWidth - step) {
+        const sliderWidth = window.innerWidth < 768 ? slider.offsetWidth : slider.offsetWidth / 2;
+
+        if (targetBtn === 'next' && -sliderPosition < (currentSlide - 1) * sliderWidth - step) {
             sliderPosition = sliderPosition - step;
             slider.style.transform = `translateX(${sliderPosition}px)`;
 
             requestAnimationFrame(countStep);
-        } else if (targetBtn === 'prev' && -sliderPosition > (currentSlide - 1) * slider.offsetWidth + step) {
+        } else if (targetBtn === 'prev' && -sliderPosition > (currentSlide - 1) * sliderWidth + step) {
             sliderPosition = sliderPosition + step;
             slider.style.transform = `translateX(${sliderPosition}px)`;
 
             requestAnimationFrame(countStep);
         } else {
-            sliderPosition = -(currentSlide - 1) * slider.offsetWidth;
+            sliderPosition = -(currentSlide - 1) * sliderWidth;
             slider.style.transform = `translateX(${sliderPosition}px)`;
         }
+    }
+
+    /**
+     * The position of the slider is recalculated when the width of the slider is changed.
+     */
+    function trackSliderResizing() {
+        window.addEventListener('resize', () => {
+            if (window.innerWidth < 768) {
+                sliderPosition = -(currentSlide - 1) * slider.offsetWidth;
+                slider.style.transform = `translateX(${sliderPosition}px)`;
+            } else {
+                sliderPosition = -(currentSlide - 1) * slider.offsetWidth / 2;
+                slider.style.transform = `translateX(${sliderPosition}px)`;
+            }
+        });
     }
 }
