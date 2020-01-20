d39bf2a7828b97476a3198ca423450fa8aaa5136
Olga Bolotova <olbolot@gmail.com>
2020-01-20 20:44:11 +0300

    Add matching menu items to sections

--- src/scripts/headerFixating.ts
+++ src/scripts/headerFixating.ts
@@ -9,22 +9,40 @@ interface HeaderOptions {
 export default function headerFixating(options: HeaderOptions) {
     const activeClass = 'is-active';
     const header = options.header;
-    const startPoint =  header.getBoundingClientRect().bottom;
+    const headerHeight = header.offsetHeight;
+    const startPoint = 50;
+    const links = header.querySelectorAll('.js-menu-link');
 
     window.addEventListener('scroll', toggleActiveClass);
+
+    selectActiveItem();
 
     /**
      * The class add to the header if the top of the page is below the bottom of the header, and remove if above.
      */
     function toggleActiveClass() {
-        startPoint <= window.pageYOffset
+        window.pageYOffset >= startPoint
             ? header.classList.add(activeClass)
             : header.classList.remove(activeClass);
     }
 
+    /**
+     * The menu item is become active if the page is on the matching section.
+     */
     function selectActiveItem() {
-        startPoint <= window.pageYOffset
-            ? header.classList.add(activeClass)
-            : header.classList.remove(activeClass);
+        for (const link of links) {
+            const linkHref = link.getAttribute('href');
+            const section: HTMLElement = document.querySelector(linkHref);
+            const sectionTopPosition = section.getBoundingClientRect().top + window.pageYOffset - headerHeight;
+            const sectionBottomPosition = sectionTopPosition + section.clientHeight;
+
+            window.addEventListener('scroll', () => {
+                const position = window.pageYOffset;
+
+                position >= headerHeight && position >= sectionTopPosition && position <= sectionBottomPosition
+                    ? link.classList.add('is-active')
+                    : link.classList.remove('is-active');
+            });
+        }
     }
 }
